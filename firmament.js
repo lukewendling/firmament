dbe884f562d0a44354b65d9fa73081cd85596188
jreeme <john.reeme@gmail.com>
2015-05-12 17:28:27 -0500

    Quick commit.

--- firmament.js
+++ firmament.js
@@ -2,14 +2,6 @@
 
 var configFilename = 'firmament.json';
 
-var configFileTemplate = [
-    {
-        containerName: '<what would you like to name your container?>',
-        imageName: '<which Docker image would you like to use to name this container?>',
-        hostName: '<what would you like the hostname of your container to be?>'
-    }
-];
-
 var moduleDependencies = {
     "docker-remote-api": "^4.4.1",
     "command-line-args": "^0.5.9",
@@ -20,7 +12,90 @@ var moduleDependencies = {
     "prompt-sync": "^1.0.0",
     "single-line-log": "^0.4.1",
     "yesno": "^0.0.1",
+    "nimble": "^0.0.2",
+    "deep-extend": "^0.4.0",
     "util": "^0.10.3"
+};
+
+var configFileTemplate = [
+    {
+        name: 'mongo',
+        Image: 'mongo',
+        Hostname: 'mongo'
+    },
+    {
+        name: 'swagger',
+        Image: 'strongloop/strong-pm',
+        Hostname: 'swagger',
+        HostConfig: {
+            Links: ['mongo:mongo'],
+            PortBindings: {
+                '3000/tcp': [{HostPort: '3000'}],
+                '8701/tcp': [{HostPort: '8701'}]
+            }
+        }
+    }
+];
+
+var containerFullConfig = {
+    Image: '',
+    Hostname: '',
+    Domainname: '',
+    User: '',
+    AttachStdin: false,
+    AttachStdout: true,
+    AttachStderr: true,
+    Tty: false,
+    OpenStdin: false,
+    StdinOnce: false,
+    Env: ['ENV0=how now brown cow', 'ENV1=320'],
+    Cmd: [],
+    Entrypoint: '',
+    Labels: {
+        'version': '1.0'
+    },
+    Volumes: {
+        '/tmp': {}
+    },
+    WorkingDir: '',
+    NetworkDisabled: false,
+    MacAddress: '',
+    ExposedPorts: {
+        '22/tcp': {}
+    },
+    SecurityOpts: [''],
+    HostConfig: {
+        Binds: null,
+        BindsExample: ['/tmp:/tmp'],
+        Links: null,
+        LinksExample: ['redis:redis'],
+        LxcConf: {'lxc.utsname': 'docker'},
+        Memory: 0,
+        MemorySwap: 0,
+        CpuShares: 512,
+        CpusetCpus: null,
+        PortBindings: null,
+        PortBindingsExample: {'22/tcp': [{'HostPort': '11022'}]},
+        PublishAllPorts: false,
+        Privileged: false,
+        ReadonlyRootfs: false,
+        Dns: null,
+        DnsExample: ['8.8.8.8', '9.9.9.9'],
+        DnsSearch: null,
+        ExtraHosts: null,
+        ExtraHostsExample: ['localhost:127.0.0.1'],
+        VolumesFrom: null,
+        VolumesFromExample: ['containerName[:<ro|rw>]'],
+        CapAdd: ['NET_ADMIN'],
+        CapDrop: ['MKNOD'],
+        RestartPolicy: {'Name': '', 'MaximumRetryCount': 0},
+        RestartPolicyExample: {'Name': '<always|on-failure>', 'MaximumRetryCount': 0},
+        NetworkMode: 'bridge',
+        Devices: null,
+        Ulimits: null,
+        LogConfig: {'Type': 'json-file', Config: {}},
+        CgroupParent: ''
+    }
 };
 
 var requireCache = {};
@@ -92,7 +167,7 @@ function letThereBeLight() {
     if (doSubCommand) {
         return;
     }
-    if (!doSubCommand && commander.template) {
+    if (commander.template) {
         //User has specified '-t'
         var checkType = requireCache['check-types'];
         var templateFilename = checkType.string(commander.template) ? commander.template : configFilename;
@@ -113,7 +188,7 @@ function letThereBeLight() {
     } else {
         var jsonFile = requireCache['jsonfile'];
         //Let's try to set things up according to config files
-        var configFiles = commander.args || [configFilename];
+        var configFiles = commander.args.length ? commander.args : [configFilename];
         var containerConfigs = [];
         configFiles.forEach(function (configFile) {
             try {
@@ -148,7 +223,7 @@ function enterDockerCmdProcessor(cmd, options) {
         case('ps'):
             dockerListContainers(options, function (err, containers, callMeBack) {
                 console.log(containers);
-                if(callMeBack){
+                if (callMeBack) {
                     enterDockerCmdProcessor();
                 }
             });
@@ -167,14 +242,95 @@ function dockerListContainers(options, cb) {
 }
 
 function processContainerConfigs(containerConfigs) {
+    var deepExtend = requireCache['deep-extend'];
     var docker = requireCache['docker-remote-api'];
     var request = docker({host: '/var/run/docker.sock'});
-    request.get('/images/json?all=1', {json: true}, function (err, images) {
-        if (err) {
-            fatal(err);
+    var sortedContainerConfigs = dependencySort(containerConfigs);
+    var functionArray = [];
+
+    /*    request.post('/containers/7aa8/start', {json:{Binds:['/tmp:/tmp']}}, function (err, result) {
+     console.log(err || result);
+     });
+     return;*/
+    sortedContainerConfigs.forEach(function (containerConfig) {
+        functionArray.push(function (callback) {
+            var fullContainerConfigCopy = {};
+            deepExtend(fullContainerConfigCopy, containerFullConfig);
+            deepExtend(fullContainerConfigCopy, containerConfig);
+            var queryString = {name: fullContainerConfigCopy.name};
+            request.post('/containers/create', {
+                json: fullContainerConfigCopy,
+                qs: queryString
+            }, function (err, result) {
+                console.log(err || result);
+                callback();
+            });
+        });
+    });
+    var nimble = requireCache['nimble'];
+    var containers = [];
+    nimble.series([
+        function (callback) {
+            nimble.parallel(functionArray, function () {
+                callback();
+            });
+        },
+        function (callback) {
+            dockerListContainers({all: true}, function (err, res) {
+                containers = res;
+                callback();
+            });
+        },
+        function (callback) {
+            functionArray = [];
+            sortedContainerConfigs.forEach(function (containerConfig) {
+                var testName = '/' + containerConfig.name;
+                containers.forEach(function (container) {
+                    container.Names.forEach(function (name) {
+                        if (testName === name) {
+                            functionArray.push(function (callback) {
+                                var restCmd = '/containers/' + container.Id + '/start';
+                                request.post(restCmd, {json: {Dns: null}}, function (err, result) {
+                                    console.log(err || result);
+                                    callback();
+                                });
+                            });
+                        }
+                    });
+                });
+            });
+            nimble.series(functionArray, callback);
         }
-        console.log('hello'.green);
-        console.log(images);
+    ]);
+}
+
+function startContainersSerially(containerConfigs) {
+    var containerConfig = containerConfigs.shift();
+    if (!containerConfig) {
+        return;
+    }
+    dockerListContainers({all: true}, function (err, containers) {
+        var testName = '/' + containerConfig.name;
+        for (var i = 0; i < containers.length; ++i) {
+            var container = containers[i];
+            for (var j = 0; j < container.Names.length; ++j) {
+                var name = container.Names[j];
+                if (testName === name) {
+                    var docker = requireCache['docker-remote-api'];
+                    var request = docker({host: '/var/run/docker.sock'});
+
+                    var restCmd = '/containers/' + container.Id + '/start';
+                    request.post(restCmd, function (err, result) {
+                        if (err) {
+                            fatal(err);
+                        }
+                        console.log(result);
+                        startContainersSerially(containerConfigs);
+                    });
+                    return;
+                }
+            }
+        }
     });
 }
 
@@ -192,6 +348,67 @@ function writeJsonObjectToFileThenQuit(path, obj) {
     exit(0);
 }
 
+function dependencySort(containerConfigs) {
+    var sortedContainerConfigs = [];
+    //Sort on linked container dependencies
+    var objectToSort = {};
+    var containerConfigByNameMap = {};
+    containerConfigs.forEach(function (containerConfig) {
+        if (containerConfigByNameMap[containerConfig.name]) {
+            fatal({Message: 'Same name is used by more than one container.'})
+        }
+        containerConfigByNameMap[containerConfig.name] = containerConfig;
+        var dependencies = [];
+        if (containerConfig.HostConfig && containerConfig.HostConfig.Links) {
+            containerConfig.HostConfig.Links.forEach(function (link) {
+                var linkName = link.split(':')[0];
+                dependencies.push(linkName);
+            });
+        }
+        objectToSort[containerConfig.name] = dependencies;
+    });
+    var sortedContainerNames = topologicalDependencySort(objectToSort);
+    sortedContainerNames.forEach(function (sortedContainerName) {
+        sortedContainerConfigs.push(containerConfigByNameMap[sortedContainerName]);
+    });
+    return sortedContainerConfigs;
+}
+
+function topologicalDependencySort(graph) {
+    var sorted = [], // sorted list of IDs ( returned value )
+        visited = {}; // hash: id of already visited node => true
+
+    // 2. topological sort
+    try {
+        Object.keys(graph).forEach(function visit(name, ancestors) {
+            // if already exists, do nothing
+            if (visited[name]) {
+                return
+            }
+
+            if (!Array.isArray(ancestors)) {
+                ancestors = []
+            }
+            ancestors.push(name);
+            visited[name] = true;
+
+            var deps = graph[name];
+            deps.forEach(function (dep) {
+                if (ancestors.indexOf(dep) >= 0) {
+                    fatal({Message: 'Circular dependency "' + dep + '" is required by "' + name + '": ' + ancestors.join(' -> ')});
+                }
+
+                visit(dep, ancestors.slice(0)); // recursive call
+            });
+
+            sorted.push(name);
+        });
+    } catch (ex) {
+        fatal({Message: 'Linked container dependency sort failed. You are probably trying to link to an unknown container.'});
+    }
+    return sorted;
+}
+
 function fatal(ex) {
     console.log("\nMan, sorry. Something bad happened and I can't continue. :(".yellow)
     console.log("\nHere's all I know:\n".yellow);
@@ -203,8 +420,4 @@ function fatal(ex) {
 function exit(code) {
     process.exit(code);
 }
-
-//var shell = require('shelljs');
-//var result = shell.exec('docker run --detach --restart=no --name mongo --hostname mongo mongo', {silent: true});
-//shell.echo(result);
 
