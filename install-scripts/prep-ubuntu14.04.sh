7e8b86a77d585005253a0144d002ca80a29e951f
my name <my@email.com>
2015-06-17 14:52:01 -0500

    Add update-alternatives so vim will be the editor for git commits

--- install-scripts/prep-ubuntu14.04.sh
+++ install-scripts/prep-ubuntu14.04.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+update-alterantives --config editor
 echo 'set -o vi' >> ~/.bashrc
 echo "alias d='/usr/bin/docker'" >> ~/.bashrc
 echo "alias f='~/firmament/firmament.js'" >> ~/.bashrc
