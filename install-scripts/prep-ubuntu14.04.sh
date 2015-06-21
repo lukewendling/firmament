146c1b57fc37bcdaeb28406d6de7a44a9cc68779
jreeme <john.reeme@gmail.com>
2015-06-21 15:05:53 -0500

    Get Docker 1.6 from someplace else

--- install-scripts/prep-ubuntu14.04.sh
+++ install-scripts/prep-ubuntu14.04.sh
@@ -11,7 +11,11 @@ git config --global user.name "user nowhere"
 apt-get install -y build-essential
 wget http://nodejs.org/dist/v0.12.4/node-v0.12.4.tar.gz
 tar xvf node-v0.12.4.tar.gz
-wget -qO- https://get.docker.com/ | sh
+#wget -qO- https://get.docker.com/ | sh
+echo deb http://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+apt-key adv --keyserver pgp.mit.edu --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+apt-get update
+apt-get install -y lxc-docker-1.6.0
 cd node-v0.12.4
 ./configure
 make
