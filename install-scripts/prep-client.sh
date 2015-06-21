0a12aaece1bcb1d1f28e3157a1a33e78da3aa249
user nowhere <user@nowhere.com>
2015-06-21 15:46:03 -0500

    A

---
+++ install-scripts/prep-client.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+update-alternatives --config editor
+echo 'set -o vi' >> ~/.bashrc
+echo "alias d='/usr/bin/docker'" >> ~/.bashrc
+echo "alias f='~/firmament/firmament.js'" >> ~/.bashrc
+echo 'set nu' >> ~/.vimrc
+git config --global user.email "user@nowhere.com"
+git config --global user.name "user nowhere"
