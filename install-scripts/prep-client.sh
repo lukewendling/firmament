9c73e0c55ec9882bfe4a586661ba2d6efbea1ea8
user nowhere <user@nowhere.com>
2015-06-22 11:34:08 -0500

    Add CI=true so bower won't ask for feedback

--- install-scripts/prep-client.sh
+++ install-scripts/prep-client.sh
@@ -6,3 +6,5 @@ echo "alias f='~/firmament/firmament.js'" >> ~/.bashrc
 echo 'set nu' >> ~/.vimrc
 git config --global user.email "user@nowhere.com"
 git config --global user.name "user nowhere"
+#Continuous integration so bower won't ask for statistics
+export CI=true
