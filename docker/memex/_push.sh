71c6c38f5d1935ef65d964c8bd1767334dc330cd
John Reeme <john.reeme@soteradefense.com>
2015-07-25 11:01:07 -0500

    Add files to create containers for Summer Camp delivery

---
+++ docker/memex/_push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker push memex/data-container:7.0
+docker push memex/mysql:7.0
+docker push memex/mongo:7.0
+docker push memex/loopback:7.0
+docker push memex/webapp:7.0
+docker push memex/tangelo:7.0
