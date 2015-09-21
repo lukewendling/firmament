fdcd4eb3ec64fac0bb4b4f069242bb0111d17084
j <j@b.c>
2015-09-21 15:00:49 -0500

    datawake script

--- install-scripts/datawake.pl
+++ install-scripts/datawake.pl
@@ -1,2 +1,15 @@
 #!/usr/bin/perl
-sudo curl -sSL https://get.docker.com/ | sh
+#$which_docker = `which docker`;
+#if($which_docker !~ /docker/){
+#	print `sudo curl -sSL https://get.docker.com/ | sh`;
+#}
+#$NEW_USER="datawake";
+#print `sudo useradd -m -d /home/$NEW_USER -N -G adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare,docker -s /bin/bash $NEW_USER`;
+#print `sudo passwd $NEW_USER`;
+
+print `docker run -dt --name data-container -h data-container jreeme/data-container:7.0`;
+print `docker run -dt --volumes-from data-container --name mongo -h mongo jreeme/mongo:7.0`;
+print `docker run -dt --volumes-from data-container --name mysql -h mysql jreeme/mysql:7.0`;
+print `docker run -dt --name loopback -h loopback --link mongo:mongo --link mysql:mysql -p 8701:8701 -p 3001:3001 jreeme/loopback:7.0`;
+print `docker run -dt --name webapp -h webapp --link loopback:loopback -p 8702:8701 -p 3002:3001 jreeme/webapp:7.0`;
+print `docker run -dt --name tangelo -h tangelo --link mysql:mysql -p 80:80 jreeme/tangelo:7.0`;
