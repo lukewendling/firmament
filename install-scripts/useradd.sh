877158b3e8a5be79d86e6c71cf9b1d34c813430a
Justin <justin.lueders@gmail.com>
2015-11-02 23:23:27 -0600

    adding strong-pm to the user add script

--- install-scripts/useradd.sh
+++ install-scripts/useradd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NEW_USER="docker"
-useradd -m -d /home/$NEW_USER -N -G adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare,docker -s /bin/bash $NEW_USER
+useradd -m -d /home/$NEW_USER -N -G adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare,docker,strong-pm -s /bin/bash $NEW_USER
 passwd $NEW_USER
